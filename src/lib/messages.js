5592455b7a04b9b281f696ddd62b228e6dc4d9ee
Jonathan Fox <jf162@icloud.com>
2025-04-08 10:49:23 +0100

    added instructions

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -1,6 +1,27 @@
 const color = "\x1b[1;35m";
 const normal = "\x1b[0m";
 export const introMessage = [
+    "+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
+	"|                                                                             |",
+	"| WebVM is a virtual Linux environment running in the browser via WebAssembly |",
+	"|                                                                             |",
+	"| WebVM is powered by the CheerpX virtualization engine, which enables safe,  |",
+	"| sandboxed client-side execution of x86 binaries, fully client-side          |",
+	"|                                                                             |",
+	"| CheerpX includes an x86-to-WebAssembly JIT compiler, a virtual block-based  |",
+	"| file system, and a Linux syscall emulator                                   |",
+	"|                                                                             |",
+	"| [News] Integrating Claude Computer Use in WebVM!                            |",
+	"|                                                                             |",
+	"| " + underline + "https://labs.leaningtech.com/blog/webvm-claude" + normal + "                              |",
+	"|                                                                             |",
+	"| [7 - 14 April] WebVM Hackathon II is live! Join to win 500£!                |",
+	"|                                                                             |",
+	"| " + underline + "https://cheerpx.io/hackathon" + normal + "                                                |",
+	"|                                                                             |",
+	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
+    "",
+    "",
     "                                  __,='`````'=/__",
     "                                  '//  (o) \\(o) \\ `",
     "                                  //|    ,_)   (`\\   _,-,",
@@ -21,8 +42,12 @@ export const introMessage = [
     "                                   \\,---_|    |_---./",
     "                                   ooOO(_)    (_)OOoo",
     "",
-    color + "                         LINUX DUNGEON GAME" + normal,
-    ""
+    color + "              WELCOME TO THE LINUX DUNGEON GAME" + normal,
+    "",
+    "",
+    color + + underline + "          TAKE A LOOK IN YOUR HOME DIR" + normal,
+    color + + underline + "EACH ROOM REQUIRES YOU TO USE LINUX COMMANDS TO FIND A KEY" + normal,
+    color + + underline + "TRY STARTING WITH ls - GOOD LUCK ON YOUR ADVENTURE" + normal,
 ];
 export const errorMessage = [
     color + "CheerpX could not start" + normal,
