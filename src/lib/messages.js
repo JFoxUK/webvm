c6d59cf36d6bba27ed24967af5642ac1e802347e
Jonathan Fox <jf162@icloud.com>
2025-04-08 10:52:44 +0100

    removed underline

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -45,9 +45,9 @@ export const introMessage = [
     color + "              WELCOME TO THE LINUX DUNGEON GAME" + normal,
     "",
     "",
-    color + + underline + "          TAKE A LOOK IN YOUR HOME DIR" + normal,
-    color + + underline + "EACH ROOM REQUIRES YOU TO USE LINUX COMMANDS TO FIND A KEY" + normal,
-    color + + underline + "TRY STARTING WITH ls - GOOD LUCK ON YOUR ADVENTURE" + normal,
+    color + "          TAKE A LOOK IN YOUR HOME DIR" + normal,
+    color + "EACH ROOM REQUIRES YOU TO USE LINUX COMMANDS TO FIND A KEY" + normal,
+    color + "TRY STARTING WITH ls - GOOD LUCK ON YOUR ADVENTURE" + normal,
 ];
 export const errorMessage = [
     color + "CheerpX could not start" + normal,
